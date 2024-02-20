69d3f319871aeae685d469a1b81a996c80b24eb8
Tan Le Minh <minhtan1581@gmail.com>
2024-02-20 10:02:01 +0700

    Refactor clients table schema

--- database/migrations/2024_01_06_012845_create_clients_table.php
+++ database/migrations/2024_01_06_012845_create_clients_table.php
@@ -13,53 +13,27 @@ return new class extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->uuid('token')
-                ->default(\Illuminate\Support\Facades\DB::raw('(UUID())'));
-            $table->foreignId('country_id')
-                ->nullable();
-            $table->foreignId('event_id')
-                ->nullable(false);
-            $table->string('event_code')
-                ->nullable(false);
-            $table->string('qrcode', 200)
-                ->nullable(false);
-            $table->unique(['event_id', 'qrcode']);
-            $table->string('email', 255)
-                ->nullable();
-            $table->string('phone', 50)
-                ->nullable();
-            $table->json('file_collection')
-                ->nullable();
-            $table->json('custom_fields')
-                ->nullable();
-            $table->string('type', 50)
-                ->default('NORMAL')
-                ->nullable(false);
-            $table->string('status', 50)
-                ->default('NEW')
-                ->nullable(false);
-            $table->foreignId('created_by')
-                ->nullable();
-            $table->foreignId('updated_by')
-                ->nullable();
+            $table->foreignId('event_id')->nullable(false);
+            $table->string('email', 100)->nullable();
+            $table->string('phone', 20)->nullable();
+            $table->string('address', 255)->nullable();
+            $table->string('type', 50)->default('NORMAL')->nullable(false);
+            $table->string('status', 50)->default('NEW')->nullable(false);
+            $table->foreignId('created_by')->nullable();
+            $table->foreignId('updated_by')->nullable();
             $table->timestamps();
+
+            $table->unique(['event_id', 'phone']);
 
             /* INDEX */
             $table->index('id', 'idx_id');
             $table->index('event_id', 'idx_event_id');
-            $table->index('event_code', 'idx_event_code');
-            $table->index('country_id', 'idx_country_id');
-            $table->index('qrcode', 'idx_qrcode');
 
             /* RELATIONSHIP */
             $table->foreign('event_id')
                 ->references('id')
                 ->on('events')
                 ->onDelete('restrict');
-            $table->foreign('country_id')
-                ->references('id')
-                ->on('countrys')
-                ->onDelete('set null');
             $table->foreign('created_by')
                 ->references('id')
                 ->on('users')
