4e404641ad86832a51edd05b638728a124d77195
Minh Tân Lê <tan.lm@fibo.vn>
2024-02-28 00:22:40 +0700

    Refactor EventCollection to hide specific columns

--- app/Http/Resources/Event/EventCollection.php
+++ app/Http/Resources/Event/EventCollection.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Resources\Event;
 
-use App\Http\Resources\BaseCollection;
 use Illuminate\Http\Request;
+use App\Http\Resources\BaseCollection;
 
 class EventCollection extends BaseCollection
 {
+    protected $hiddenColumns = [
+        'email_content',
+        'cards_content',
+        'main_fields',
+        'custom_fields',
+    ];
     /**
      * Transform the resource collection into an array.
      *
@@ -16,8 +22,20 @@ class EventCollection extends BaseCollection
     {
         return [
             'count'         => $this->collection->count(),
-            'collection'    => parent::toArray($request),
+            'collection'    => $this->finalizeCollection(),
             'pagination'    => $this->getPaginateMeta(),
         ];
+    }
+
+    public function finalizeCollection()
+    {
+        return $this->collection->map(function ($event) {
+            $data = $event->resource->toArray();
+
+            foreach ($this->hiddenColumns as $column) {
+                unset($data[$column]);
+            }
+            return $data;
+        });
     }
 }
