8a95991aad633cc3e1152eda417897d126865ee4
Minh Tân Lê <tan.lm@fibo.vn>
2024-03-06 21:56:38 +0700

    Add 'fullname' column to clients table

--- database/migrations/2024_01_06_012845_create_clients_table.php
+++ database/migrations/2024_01_06_012845_create_clients_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->foreignId('event_id')->nullable(false);
+            $table->string('fullname', 100)->nullable(false);
             $table->string('email', 100)->nullable();
             $table->string('phone', 20)->nullable();
             $table->string('address', 255)->nullable();
