e916380d4acbfb8b7b60d66a34a0c34d4673efa1
Tan Le Minh <minhtan1581@gmail.com>
2024-03-31 08:53:17 +0700

    Add new permissions migration

---
+++ database/migrations/2024_03_31_084809_add_new_permission.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $permissions = [
+                'post:view',
+                'post:create',
+            ];
+
+            foreach ($permissions as $permission) {
+                $exists = DB::table('permissions')->where('name', $permission)->exists();
+                if (!$exists) {
+                    DB::table('permissions')->insert([
+                        'name'          => $permission,
+                        'guard_name'    => 'api',
+                        'created_at'    => now(),
+                    ]);
+                }
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
